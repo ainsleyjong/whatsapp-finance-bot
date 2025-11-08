55de826840e8e73194454ee60761db4e5e2ae7c0
ainsleyjong <ajz2475@gmail.com>
2025-11-08 18:30:19 +0000

    refactor: reorganize project directory structure

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from config import SYMBOLS, INDUSTRIES, MAX_PAGES
-from news import extract_data, print_articles
+from app.services.market_news import extract_data, print_articles
+
+SYMBOLS = "AAPL,MSFT,GOOGL,NVDA,AMZN,TSLA"
+INDUSTRIES = "Technology,Industrials,Financial Services"
 
 if __name__ == "__main__":
     if SYMBOLS:
-        symbol_articles = extract_data(pages=MAX_PAGES, symbols=SYMBOLS)
+        symbol_articles = extract_data(pages=1, symbols=SYMBOLS)
         print_articles(symbol_articles, header="📈 Articles by SYMBOLS")
         
     if INDUSTRIES:
-        industry_articles = extract_data(pages=MAX_PAGES, industries=INDUSTRIES)
+        industry_articles = extract_data(pages=1, industries=INDUSTRIES)
         print_articles(industry_articles, header="🏭 Fetching articles by INDUSTRIES")
